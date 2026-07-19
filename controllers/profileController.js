24e8515cdc78bbbebda35a6046fd2da59569352a
unknown <komalpreetkaur202421@gmail.com>
2026-07-19 18:05:42 +0530

    fix: match case-sensitive file paths for linux deployment

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/user");
 
 // GET Profile
 const getProfile = async (req, res) => {
--- index.js
+++ index.js
@@ -39,6 +39,8 @@ app.get("/", (req, res) => {
 });
 
 // Start Server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
